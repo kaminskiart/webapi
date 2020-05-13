19f38657ec30ba365143cc3f05a50cba54d854af
Artur Kamiński <artur.kaminski@simeo.io>
2020-05-13 15:40:15 +0200

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple web api with swagger configuration
---
+++ WebApplicationEG/Controllers/UsersController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationEG.Models;
+
+namespace WebApplicationEG.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public UsersController(UserContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get the list of all Users.
+        /// </summary>
+        /// <returns>The list of Users.</returns>
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        /// <summary>
+        /// Get the User by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>The User by Id.</returns>
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        /// <summary>
+        /// Creates a User.
+        /// </summary>
+        /// /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/Users
+        ///     {
+        ///         "id": 1,
+        ///         "name": "TestName",
+        ///         "lastName": "TestLastName"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="user"></param>
+        /// <returns>A newly created User</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>  
+        // POST: api/Users
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        private bool UserExists(long id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
